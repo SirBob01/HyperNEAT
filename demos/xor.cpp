cd9bc62fffe42fc891202ab6d88cd190f043b5dd
SirBob01 <kleonardo52@gmail.com>
2022-04-29 00:13:00 +1000

    Refactor xor demo to show average generation where solution is found

--- demos/xor.cpp
+++ demos/xor.cpp
@@ -25,36 +25,49 @@ int main() {
     params.phenome_params.band_threshold = 0.1;
     params.phenome_params.initial_depth = 1;
     params.phenome_params.maximum_depth = 2;
-    params.max_stagnation = 1;
-    HyperNEAT::Pool pool(input, output, params);
+    params.max_stagnation = 2;
 
-    for (int i = 0; i < max_generations; i++) {
-        std::cout << "Generation " << pool.get_generations() << "\n";
+    float total_generations = 0;
+    const float total_runs = 100;
+    for (int run = 0; run < total_runs; run++) {
+        int gen;
+        HyperNEAT::Pool pool(input, output, params);
+        for (gen = 0; gen < max_generations; gen++) {
+            // Calculate the fitness of each network
+            pool.evolve([&testcases](HyperNEAT::Phenome &phenome) {
+                double total_error = 0.0;
+                for (auto &c : testcases) {
+                    double result = phenome.forward(c.input)[0];
+                    double diff = c.output[0] - result;
+                    total_error += diff * diff;
+                }
+                phenome.set_fitness(1 / (1 + std::sqrt(total_error)));
+            });
 
-        // Calculate the fitness of each network
-        pool.evolve([&testcases](HyperNEAT::Phenome &phenome) {
-            double total_error = 0.0;
-            for (auto &c : testcases) {
-                double result = phenome.forward(c.input)[0];
-                double diff = c.output[0] - result;
-                total_error += diff * diff;
+            // Print the result of the fittest phenome
+            HyperNEAT::Phenome phenome = pool.get_global_fittest();
+            if (std::round(phenome.forward({0, 0})[0]) == 0 &&
+                std::round(phenome.forward({1, 1})[0]) == 0 &&
+                std::round(phenome.forward({0, 1})[0]) == 1 &&
+                std::round(phenome.forward({1, 0})[0]) == 1) {
+                double total_error = 0.0;
+                for (auto &c : testcases) {
+                    double result = phenome.forward(c.input)[0];
+                    double diff = c.output[0] - result;
+                    total_error += diff * diff;
+                    std::cout << c.input[0] << " ^ " << c.input[1] << " = "
+                              << result << "\n";
+                }
+                double accuracy = 1 / (1 + std::sqrt(total_error));
+                std::cout << "Accuracy " << accuracy << " | Generation " << gen
+                          << "\n\n";
+                break;
             }
-            phenome.set_fitness(1 / (1 + std::sqrt(total_error)));
-        });
-
-        // Print the result of the fittest phenome
-        HyperNEAT::Phenome phenome = pool.get_global_fittest();
-        double total_error = 0.0;
-        for (auto &c : testcases) {
-            double result = phenome.forward(c.input)[0];
-            double diff = c.output[0] - result;
-            total_error += diff * diff;
-            std::cout << c.input[0] << " ^ " << c.input[1] << " = " << result
-                      << "\n";
         }
-
-        double accuracy = 1 / (1 + std::sqrt(total_error));
-        std::cout << "Accuracy " << accuracy << "\n\n";
+        total_generations += gen;
     }
+    std::cout << "Solution found in " << (total_generations / total_runs)
+              << " generations on average\n";
+
     return 0;
 }
